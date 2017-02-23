b9b590d8cfdfd74e3718cccffa054c683a401c9f
danoyu <danoyu@msn.com>
2017-02-23 15:11:30 +0200

    Update oneClick.py

--- Training/oneClick.py
+++ Training/oneClick.py
@@ -25,7 +25,7 @@ def exe_1_level(file_name, nb):
 
 # print(classifier.list_classifiers_name_id())
 # classifier.delete_by_id_classifiers()
-exe_1_level(filename, nb)  
+#exe_1_level(filename, nb)  
 
 def exe_2_levels(file_name, nb):
     # print(filename)
